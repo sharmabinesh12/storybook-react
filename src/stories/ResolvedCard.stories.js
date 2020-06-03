9b25d8ab48bab555e2a25fd98a943e062a9a2593
Binesh Kumar <binesh@enterprisebot.org>
2020-06-03 16:07:00 +0530

    recognized param handled for nes training

---
+++ src/stories/ResolvedCard.stories.js
@@ -0,0 +1,82 @@
+import React,{ useState } from 'react';
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+
+import RecognizedValueCard from '../components/recognizedValueCard';
+import Edit_Logo from "./img/ic_edit_black.svg";
+import Save_Logo from "./img/save_black.svg";
+
+
+export default {
+  title: 'RecognizedValueCard',
+  component: RecognizedValueCard,
+};
+
+storiesOf('RecognizedValueCard', module)
+  .add('with edit', () =>{
+		const [isEdit, setIsEdit] = useState(false);
+		return (
+			<div className="listHeader-box">
+				<RecognizedValueCard 
+					background = {'grey'}
+					color = {'#fff' }
+					displayName = {'city destination'}
+					isEdit = {isEdit}
+					value = {'Bangalore'}
+					resolved = {true}
+					handleEditParamChange = {action('handle edit param change')} 
+					handleEditDone = {()=>setIsEdit(false)} 
+					editResolvedParam = {()=>setIsEdit(true)}
+					handleDeleteParamConfirmation = {action('param deleted')}
+					Save_Logo = { Save_Logo}
+					Edit_Logo = {Edit_Logo}
+				/>
+			</div>
+		)
+	})
+
+	.add('without resolved', () =>{
+		const [isEdit, setIsEdit] = useState(false);
+		return (
+			<div className="listHeader-box">
+				<RecognizedValueCard 
+					background = {'grey'}
+					color = {'#fff' }
+					displayName = {'city destination'}
+					isEdit = {isEdit}
+					value = {'Bangalore'}
+					resolved = {false}
+					handleEditParamChange = {action('handle edit param change')} 
+					handleEditDone = {()=>setIsEdit(false)} 
+					editResolvedParam = {()=>setIsEdit(true)}
+					handleDeleteParamConfirmation = {action('param deleted')}
+					Save_Logo = { Save_Logo}
+					Edit_Logo = {Edit_Logo}
+				/>
+			</div>
+		)
+	})
+
+	.add('without edit', (state, setState) => (
+    <div className="listHeader-box">
+			<RecognizedValueCard 
+				background = {'#ddd000'}
+				color = {'#fff' }
+				displayName = {'city destination'}
+				isEdit = {false}
+				value = {'Bangalore'}
+				resolved = {false}
+				handleEditParamChange = {action('handle edit param change')} 
+				handleEditDone = {action('handle edit param done')} 
+				editResolvedParam = {action('handle edit param')}
+				handleDeleteParamConfirmation = {action('param deleted')}
+				Save_Logo = { Save_Logo}
+			/>
+    </div>
+	)) 
+
+
+	
+
+
+
